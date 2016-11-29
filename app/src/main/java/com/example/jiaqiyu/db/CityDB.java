b8550671431957fbb5c7f585b80d96ea2d2b1fe3
Fish_Plus_Flag <jiaqiyu0828@163.com>
2016-11-29 20:58:17 +0800

    Nov29

---
+++ app/src/main/java/com/example/jiaqiyu/db/CityDB.java
@@ -0,0 +1,42 @@
+package com.example.jiaqiyu.db;
+//   android C  C++
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.jiaqiyu.bean.City;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by JiaQi Yu on 2016/10/25.
+ */
+
+public class CityDB {
+    public  static  final String CITY_DB_NAME = "city.db";
+    private static final String CITY_TABLE_NAME = "city";
+    private SQLiteDatabase db;
+
+    public CityDB(Context context, String path){
+        db = context.openOrCreateDatabase(path, Context.MODE_PRIVATE,null);
+    }
+
+    public List<City> getAllCity(){
+        List<City> list = new ArrayList<City>();
+        Cursor cursor = db.rawQuery("SELECT * from "+CITY_TABLE_NAME,null);
+        while (cursor.moveToNext()){
+            String province = cursor.getString(cursor.getColumnIndex("province"));
+            String city =cursor.getString(cursor.getColumnIndex("city"));
+            String number = cursor.getString(cursor.getColumnIndex("number"));
+            String allPY = cursor.getString(cursor.getColumnIndex("allpy"));
+            String allFirstPY = cursor.getString(cursor.getColumnIndex("allfirstpy"));
+            String firstPY = cursor.getString(cursor.getColumnIndex("firstpy"));
+            City item = new City(province,city,number,firstPY,allPY,allFirstPY);
+            list.add(item);
+        }
+        return list;
+    }
+
+}
